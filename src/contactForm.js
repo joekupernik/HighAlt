19a626db5d2084114325e53b9ee1028d219d6903
Joe Kupernik <joekupernik@gmail.com>
2023-01-30 13:30:32 -0800

    contact form validation and basic styling

--- src/contactForm.js
+++ src/contactForm.js
@@ -1,85 +1,126 @@
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-//import Card  from '@mui/material/Card';
-//import  CardMedia from '@mui/material/CardMedia';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import { Typography } from '@mui/material';
+
+
+
+
+const validationSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  phoneNumber: Yup.string()
+    .min(9, 'Too Short!')
+    .max(11, 'Too Long!')
+    .required('Required'),
+  message: Yup.string()
+    .min(2, 'Too Short')
+    .max(250, 'Too Long')
+    .required('required')
+});
+
+
 
 
 export default function ContactForm() {
- 
-  
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      message: '',
+      phoneNumber: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <Paper elevation="12" sx={{m: '10%', p: '5%'}}>
-    <Grid container  direction="row"
-        justifyContent="center"
-        alignItems="center"
+    <Paper elevation="12" sx={{ m: '10%', p: '5%' }}>
+      <Box
+        sx={{
+          '& .MuiTextField-root': { m: 1 },
+        }}
       >
-      <Typography variant="h2" p="3%" >Contact Form</Typography>
-    <Grid item>    
-    <Paper elevation={24}>
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '80%' } 
-      }}
-      noValidate
-      autoComplete="off"
-    >
-    <Grid container  direction="row"
-        justifyContent="center"
-        alignItems="center">
-    
-    <Grid item>
-        <TextField
-          label="First Name"
-          variant="standard"
-          required
-          type="text"
-        />
-        <TextField
-          label="Last Name"
-          variant="standard"
-          required
-          type="text"
-        />
-      </Grid>
-      <Grid item>
-        <TextField
-          label="Email"
-          variant="standard"
-          required
-          type="email"
-        />
-        <TextField
-          label="Phone Number"
-          variant="standard"
-          required
-          type="integer"
-        />
-      </Grid>
-      <Grid item width="80%" ml={'10%'} mt={'2%'} mb={'2%'} >
-        <TextField
-          label="Message"
-          defaultValue="How can I help?"
-          variant="outlined"
-          required
-          type="text"
-          multiline
-          rows={10}
-          fullWidth
-        />
-        <Grid item>
-       <Button size="large">Submit</Button>
-       </Grid>
-      </Grid>
-      </Grid>
-    </Box>
-    </Paper>
-    </Grid>
-    </Grid>
+        <Typography variant='h2'>Contact</Typography>
+        <div>
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              fullWidth
+              id="firstName"
+              name="firstName"
+              label="First Name"
+              variant="filled"
+              value={formik.values.firstName}
+              onChange={formik.handleChange}
+              error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+              helperText={formik.touched.firstName && formik.errors.firstName}
+            />
+            <TextField
+              fullWidth
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              variant="filled"
+              value={formik.values.lastName}
+              onChange={formik.handleChange}
+              error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+              helperText={formik.touched.lastName && formik.errors.lastName}
+            />
+            <TextField
+              fullWidth
+              id="email"
+              name="email"
+              label="Email"
+              variant="filled"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              fullWidth
+              id="phoneNumber"
+              name="phoneNumber"
+              label="Phone Number"
+              variant="filled"
+              value={formik.values.phoneNumber}
+              onChange={formik.handleChange}
+              error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
+              helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
+            />
+            <TextField
+              fullWidth
+              id="message"
+              name="message"
+              label="Message"
+              multiline
+              maxRows={4}
+              variant="filled"
+              value={formik.values.message}
+              onChange={formik.handleChange}
+              error={formik.touched.message && Boolean(formik.errors.message)}
+              helperText={formik.touched.message && formik.errors.message}
+            />
+            <Button color="primary" variant="contained" type="submit">
+              Submit
+            </Button>
+          </form>
+        </div>
+      </Box>
     </Paper>
   );
 }
