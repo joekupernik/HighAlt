5003cebbe6ccbe440c9cb1c7d5d3bac408fe35d9
Joe Kupernik <joekupernik@gmail.com>
2023-02-03 08:10:32 -0800

    changed reviews to dynamic

--- src/reviews.js
+++ src/reviews.js
@@ -1,68 +1,99 @@
 
-import Typography from "@mui/material/Typography";
-import Card  from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-//import Paper  from "@mui/material/Paper";
-//import Button from "@mui/material/Button";
-import Box  from "@mui/material/Box";
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+
+const steps = [
+  {
+    label: '- Nancy Felix - Sr Leader, Software Development - Amazon Devices',
+    description: `Joseph is a highly motivated and persistent engineer with a growing portfolio of excellent work. He is a capable engineer, good communicator and hard worker. He would be a great addition to any team looking for an engineer who is dedicated to producing quality work, will work hard and deliver.
+    `,
+    link: `https://www.linkedin.com/in/nancyfelix123/`
+  },
+  {
+    label: `- Ben Kupernik - Scientific Software Engineer - Geomega`,
+    description: `Joe is a dedicated and persistent front end engineer. He is well versed in react and JavaScript as well as CSS and the frameworks Material UI and bootstrap. We have worked on several projects together and Joe's communication was clear and easy to understand. I would recommend Joe for any position where clear and effective communication and technical skills are required.
+    `,
+    link: `https://www.linkedin.com/in/nancyfelix123/`
+  },
+  {
+    label: '- Elizabeta - Indivisible Colorado',
+    description: `Joe really helped us out with our Indivisible CO website. Before Joe, our site had clunky graphics and lots of outdated content. Joe updated our theme, added some eye-catching graphics, organized the content, and resolved some back-end issues. This was not easy because we didn't have much budget to work with, and Joe had to manage requests from our team of 8. His ability to listen to everyone, prioritize requests, and clearly communicate what could and couldn't be done was invaluable. He worked within our budget and is training us to manage minor updates on our own. I'm thrilled with the work that Joe provided and would recommend him to others.`,
+    link: `https://indivisiblecolorado.net/`
+  },
+];
+
+
 
 export default function ReviewCard() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+  const maxSteps = steps.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };  
+  
   return (
-    <Box>
-    
-      <Card variant="outlined" sx={{m: 2}}>
-          <CardContent sx={{display: "flex", justifyContent: "center"}}>
-      </CardContent>
-      <Typography  
-        component="body"
-        align="center"
-        color="text.primary"
-        padding="1%"
-        fontSize="1.5rem"
+    <Paper elevation={5} sx={{ maxWidth: '100%', flexGrow: 1}}>
+    <Paper
+      square
+      elevation={0}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        height: 70,
+        pl: 2,
+        bgcolor: 'background.default',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography>{steps[activeStep].label}</Typography>
+    </Paper>
+    <Box sx={{ height: '100%', maxWidth: '80%', width: '100%', p: 5}}>
+      {steps[activeStep].description}
+    </Box>
+    <MobileStepper
+      
+      variant="text"
+      steps={maxSteps}
+      position="static"
+      activeStep={activeStep}
+      nextButton={
+        <Button
+          size="small"
+          onClick={handleNext}
+          disabled={activeStep === maxSteps - 1}
         >
-       Joseph is a highly motivated and persistent engineer with a growing portfolio of excellent work. He is a capable engineer, good communicator and hard worker. He would be a great addition to any team looking for an engineer who is dedicated to producing quality work, will work hard and deliver.
-      </Typography>
-      
-      
-      <Typography 
-       component="body"
-       align="center"
-       color="text.primary"
-       fontSize={"1.7rem"}
-       >
-        <p> - Nancy Felix </p>
-        <a href="https://www.linkedin.com/in/nancyfelix123/"> Sr Leader, Software Development - Amazon Devices </a>
-      </Typography>
-      <CardContent sx={{display: "flex", justifyContent: "center"}}>
-      </CardContent>
-      </Card>
-     
-      
-      <Card variant="outlined" sx={{m: 2}}>
-          <CardContent sx={{display: "flex", justifyContent: "center"}}>
-      </CardContent>
-      <Typography  
-        component="body"
-        align="center"
-        color="text.primary"
-        padding="1%"
-        fontSize="1.5rem"
-        >
-       Joe is a dedicated and persistent front end engineer. He is well versed in react and JavaScript as well as CSS and the frameworks Material UI and bootstrap. We have worked on several projects together and Joe's communication was clear and easy to understand. I would recommend Joe for any position where clear and effective communication and technical skills are required.
-      </Typography>
-      
-      <Typography 
-       component="body"
-       align="center"
-       color="text.primary"
-       fontSize="1.7rem"
-       >
-        <p> - Ben Kupernik </p>
-        <a href="https://www.linkedin.com/in/nancyfelix123/"> Scientific Software Engineer - Geomega </a>
-      </Typography>
-      <CardContent sx={{display: "flex", justifyContent: "center"}}>
-      </CardContent>
-      </Card>
-      </Box>
+          Next
+          {theme.direction === 'rtl' ? (
+            <KeyboardArrowLeft />
+          ) : (
+            <KeyboardArrowRight />
+          )}
+        </Button>
+      }
+      backButton={
+        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+          {theme.direction === 'rtl' ? (
+            <KeyboardArrowRight />
+          ) : (
+            <KeyboardArrowLeft />
+          )}
+          Back
+        </Button>
+      }
+    />
+  </Paper>
   );
 }
